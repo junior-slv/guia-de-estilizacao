0634ca8d9adc57fbc5611e0f10e7ce0365ec0403
junior-slv <dirceu.junior@edu.unifil.br>
2023-04-19 11:07:39 -0300

    Initial files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container-2xl h-screen center flex justify-center bg-yellow-400">
+      App
+    </div>
+  );
+};
+
+export default App;

c3ba26096cf59f7f9b423c7da7321123d24ec31d
junior-slv <dirceu.junior@edu.unifil.br>
2023-04-21 08:37:47 -0300

    updated test style

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,35 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="container-2xl h-screen center flex justify-center bg-yellow-400">
-      App
+    <div className="container-2xl h-screen flex items-center justify-center alig bg-dark-black">
+      <div className="text-light-purple flex flex-col items-center">
+        <div>
+          <h1 className="text-2xl font-bold">H1 heading</h1>
+        </div>
+        <div className="pt-6">
+          <h2>H2 heading</h2>
+          <input
+            className="text-ice-white bg-dark-purple h-10 w-60 rounded-2xl cursor-pointer text-center"
+            type="text"
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="p-6">
+          <h2>H2 heading</h2>
+          <input
+            className="text-ice-white bg-dark-purple h-10 w-60 rounded-2xl cursor-pointer text-center"
+            type="text"
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="p-6">
+          <input
+            className="text-ice-white bg-dark-purple h-10 w-36 rounded-2xl cursor-pointer"
+            type="button"
+            value="Button"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'light-purple': '#924FD2',
+      'dark-purple': '#5E2EB1',
+      'dark-black': '#252525',
+      'ice-white': '#FFFAFA',
+    },
     extend: {},
   },
+
   plugins: [],
 }
 

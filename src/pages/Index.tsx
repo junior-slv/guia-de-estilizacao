436de284260c4bfcac3b0d030204e16094076b90
junior-slv <dirceu.junior@edu.unifil.br>
2023-04-26 09:55:21 -0300

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import logoWhite from "../assets/logo-white.png";
 const Index = () => {
   return (
     <Container>
-      <div className="justify-around w-screen grid grid-rows-2 items-center sm:grid-rows-2 font-roboto bg-purple-room bg-no-repeat md:bg-cover bg-cover bg-fixed ">
+      <div className="justify-around w-screen grid grid-rows-2 items-center sm:grid-rows-2 font-roboto bg-purple-room bg-no-repeat sm:bg-cover bg-cover bg-fixed ">
         <div className="text-ice-white flex flex-col justify-center items-center font-lato">
           <h1 className="text-4xl md:text-8xl font-bold">
             &lt; Guia de estilização /&gt;
